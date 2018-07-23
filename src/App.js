27682c5959b0b34c3ac484a025d7cfcde439b211
YannickLeRoux <rasyann@hotmail.com>
2018-07-23 11:52:25 -0400

    First build to test netlify

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
+
 import './App.css';
 
 import NavBar from './components/NavBar';
+import Services from './components/Services';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <div id="shape1" class="shape1"></div>
-      <div id="shape2" class="shape2"></div>
-      <NavBar />
-      <h1>Test H1</h1>
-      <h2>TestH2</h2>
-      <p>jskjhfkjshfjkshfjkskjfhsd</p>
-
+      <BrowserRouter>
+        <div className="App">
+        <div id="shape1" class="shape1"></div>
+        <div id="shape2" class="shape2"></div>
+        <NavBar />
+        <Services />
+        <h1>Test H1</h1>
+        <h2>TestH2</h2>
+        <p>jskjhfkjshfjkshfjkskjfhsd</p>
       </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+// import '../assets/css/Navigation.css';
 
-const NavBar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-      <div className="container">
-        <a className="navbar-brand js-scroll-trigger" href="#page-top">Azure Web Solutions</a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav text-uppercase ml-auto">
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#about">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+import {
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+   } from 'reactstrap';
 
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <header>
+        <nav className="navbar navbar-light navbar-expand-md navigation-clean">
+          <div className="container">
+            <Link className="navbar-brand" to='/'>Azure Web Solutions</Link>
+
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="nav navbar-nav ml-auto" navbar>
+
+                <NavItem>
+                  <NavLink className="nav-link" to='/services'>Services</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/portfolio'>Portfolio</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/blog'>Blog</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/contact'>Contact</NavLink>
+                </NavItem>
+
+              </Nav>
+            </Collapse>
+          </div>
+        </nav>
+      </header>
+
+    );
+  }
 }
 
 export default NavBar;
